c1fb48c49fa7313a4ff93383112e675bd2256309
muhammad abu dzar al ghifari <ghifaracc@gmail.com>
2025-09-30 10:49:45 +0700

    haha

--- test.java
+++ test.java
@@ -1,7 +1,6 @@
 public class test {
     public static void main(String[] args){
-        System.out.println("Hello,namaku Ghifari");
-        System.out.println("Hallo,namaku GHifari");
-        
+        System.out.println("Hello,saya Muhammad Abu Dzar Al Ghifari");
+
     }
 }
